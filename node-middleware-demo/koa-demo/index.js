ff62752af080d9c780375cf13d6e5503ae45c3c3
zhijs <2560418273@qq.com>
2019-11-17 22:39:14 +0800

    demo

--- node-middleware-demo/koa-demo/index.js
+++ node-middleware-demo/koa-demo/index.js
@@ -5,26 +5,33 @@ const app = new koa()
 // 中间件1
 app.use(async  (ctx, next) => {
   console.log('中间件1 start')
-   next()
+  ctx.res.state = []
+  // await next()
+  // console.log(ctx.res.state)
+  ctx.res.state.push('state1')
   console.log('中间件1 end')
   ctx.body = {
-   result: '中间件1' 
- }
+   state: ctx.res.state
+  }
 })
 
 // 中间件2
 app.use(async (ctx, next) => {
     console.log('中间件2 start')
     await next()
+    console.log(ctx.res.state)
+    ctx.res.state.push('state2')
     console.log('中间件2 end')
-    ctx.body = {
-      result: '中间件2' 
-    }
+    // ctx.body = {
+    //   result: '中间件2' 
+    // }
   })
 
   // 中间件3
 app.use(async (ctx, next) => {
     console.log('中间件3 start')
+    console.log(ctx.req.state)
+    ctx.res.state = ['state3']
     console.log('中间件3 end')
     ctx.body = {
       result: '中间件3' 
