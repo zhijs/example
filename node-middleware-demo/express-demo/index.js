c032437d6c208b5b27d9887c7485fc1e6cc0c763
yangzhi@xunlei.com <yangzhi@xunlei.com>
2019-11-22 19:47:28 +0800

    koa

--- node-middleware-demo/express-demo/index.js
+++ node-middleware-demo/express-demo/index.js
@@ -1,17 +1,16 @@
 const express = require('express')
 
 const app = new express()
-
 // 中间件1
 app.use((req, res, next) => {
   console.log('中间件1 start')
   next()
-  console.log(res.state)
-  res.state.push('state1')
-  console.log(res.state)
+  // console.log(res.state)
+  // res.state.push('state1')
+  // console.log(res.state)
   console.log('中间件1 end')
   res.json({
-   result: res.state
+   result: '中间件1'
  })
 })
 
@@ -19,22 +18,22 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
     console.log('中间件2 start')
     next()
-    // res.json({
-    //   result: '中间件2' 
-    // })
-    console.log(res.state)
-    res.state.push('state2')
+    res.json({
+      result: '中间件2' 
+    })
+    // console.log(res.state)
+    // res.state.push('state2')
     console.log('中间件2 end')
   })
 
   // 中间件3
 app.use((req, res, next) => {
     console.log('中间件3 start')
-  //   res.json({
-  //     result: '中间件3' 
-  //  })
+    res.json({
+      result: '中间件3' 
+    })
     console.log('中间件3 end')
-    res.state = ['state3']
+    // res.state = ['state3']
   })
 
   app.listen(9001)
