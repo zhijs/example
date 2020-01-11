c8f8edb678b9e35e2f29c221211c37e83d02cb8a
zhijs <2560418273@qq.com>
2020-01-11 20:38:20 +0800

    fix(model): 修改拼写

--- koa-sequlize-mysql-demo/model/index.js
+++ koa-sequlize-mysql-demo/model/index.js
@@ -1,14 +1,14 @@
-const sequlize = require('../db/index')
+const sequelize = require('../db/index')
 
 // 导入模型
-const  student = sequlize.import('./student')
-const subject = sequlize.import('./subject')
+const  student = sequelize.import('./student')
+const subject = sequelize.import('./subject')
 
 // 关联 student subject 表
 student.hasMany(subject, {foreignKey : {name: 'studentId', allowNull: false}})
 
 // 同步模型到数据库，即是创建一个 表
-sequlize.sync()
+sequelize.sync()
 // 导出模型
 exports.student = student 
 exports.subject = subject
