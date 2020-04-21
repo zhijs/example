181cbd34445aef157b7d0a18ff8102eec1af9d30
yangzhi@xunlei.com <yangzhi@xunlei.com>
2020-04-21 20:03:36 +0800

    webpack demo

---
+++ webpackDemo/entry-demo/index.js
@@ -0,0 +1,4 @@
+import test1 from './lib/test1'
+import test2 from './lib/test2'
+test1()
+test2()
---
+++ webpackDemo/entry-demo/webpack.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mode: 'development',
+  stats: 'verbose',
+  entry: ['./lib/test1.js', './lib/test2.js'], // entry - string
+}
