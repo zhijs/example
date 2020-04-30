4c24005822d97fdc26b55c48127bb8d9d31bc6b8
yangzhi@xunlei.com <yangzhi@xunlei.com>
2020-04-30 16:09:36 +0800

    webpack config

--- webpackDemo/lib/demo.js
+++ webpackDemo/lib/demo.js
@@ -1,13 +1,31 @@
+// webpack.config.js
+module.exports = {
+	// ...
+	output: {
+    libraryTarget: 'umd',
+    library:{
+      root: 'mylib',
+      amd: 'my-lib',
+      commonjs: 'my-common-lib'
+    }
+	}
+	//...
+}
+
+// bundle.js
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
+	  // commonJS2 导出
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
+	 // amd 导出
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["mylib"] = factory();
+	  // commonjs 导出
+		exports["my-common-lib"] = factory();
 	else
+	  // 全局对象属性导出
 		root["mylib"] = factory();
 })(window, function() {
-return /******/ (function(modules) { // webpackBootstrap
-})()
+  return you_lib_code_wrapper
 });
